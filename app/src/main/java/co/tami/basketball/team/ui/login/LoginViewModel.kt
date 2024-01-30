e88a6299191b3478bf2b1cc4466b27f19c512655
박상혁 <psh161@naver.com>
2024-01-31 00:10:51 +0900

    BT-01 : import 제거

--- app/src/main/java/co/tami/basketball/team/ui/login/LoginViewModel.kt
+++ app/src/main/java/co/tami/basketball/team/ui/login/LoginViewModel.kt
@@ -1,20 +1,16 @@
 package co.tami.basketball.team.ui.login
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import co.tami.basketball.team.data.repo.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(
-    private val loginRepository: LoginRepository
-) : ViewModel() {
+class LoginViewModel @Inject constructor() : ViewModel() {
 
     private val _email = MutableStateFlow("")
     val email: StateFlow<String> get() = _email.asStateFlow()
