178205f839e0497f986418bee7ea1f27975176fa
박상혁 <psh161@naver.com>
2024-02-12 16:46:01 +0900

    BT-02 : CommonUI PlayerProfileImage 추가
    - 리스트와 상세화면에서 공통적으로 사용을 위해 PlayerProfileImage CommonUI 에 추가

--- app/src/main/java/co/tami/basketball/team/ui/common/CommonUI.kt
+++ app/src/main/java/co/tami/basketball/team/ui/common/CommonUI.kt
@@ -1,9 +1,25 @@
 package co.tami.basketball.team.ui.common
 
+import android.media.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import co.tami.basketball.team.R
+import co.tami.basketball.team.ui.login.EmailOutlinedTextField
 import co.tami.basketball.team.ui.theme.BasketballTeamTheme
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun SystemThemeSurface(
@@ -11,5 +27,36 @@ fun SystemThemeSurface(
 ) {
     BasketballTeamTheme {
         Surface(color = MaterialTheme.colorScheme.background, content = content)
+    }
+}
+
+
+@Composable
+fun PlayerProfileImage(
+    profileImage: String,
+    modifier: Modifier = Modifier
+) {
+    AsyncImage(
+        modifier = modifier.clip(CircleShape),
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(profileImage)
+            .crossfade(true)
+            .build(),
+        placeholder = painterResource(id = R.drawable.ic_person_24dp),
+        contentScale = ContentScale.Crop,
+        contentDescription = null
+    )
+}
+
+@DarkLightModePreview
+@Composable
+fun PlayerProfileImagePreview() {
+    SystemThemeSurface {
+        Box(
+            modifier = Modifier
+                .padding(20.dp)
+        ) {
+            PlayerProfileImage("")
+        }
     }
 }
