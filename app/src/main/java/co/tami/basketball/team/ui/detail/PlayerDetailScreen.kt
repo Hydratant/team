13615554406edf65ea85ae074206a3cff22c6fe6
박상혁 <psh161@naver.com>
2024-02-13 23:43:33 +0900

    BT-02 : PlayerInfoBox 구현

--- app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
+++ app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
@@ -1,9 +1,15 @@
 package co.tami.basketball.team.ui.detail
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
 import co.tami.basketball.team.ui.common.DarkLightModePreview
 import co.tami.basketball.team.ui.common.SystemThemeSurface
 
@@ -20,10 +26,33 @@ fun PlayerNameText(
     )
 }
 
+@Composable
+fun PlayerInfoBox(
+    content: @Composable BoxScope.() -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .shadow(elevation = 4.dp)
+            .padding(8.dp),
+        content = content
+    )
+}
+
 @DarkLightModePreview
 @Composable
 fun DetailNamePreview() {
     SystemThemeSurface {
         PlayerNameText("루카 돈치치")
+    }
+}
+
+@DarkLightModePreview
+@Composable
+fun PlayerInfoBoxPreview() {
+    SystemThemeSurface {
+        PlayerInfoBox(
+            { Text(text = "Age가나다라") }
+        )
     }
 }
