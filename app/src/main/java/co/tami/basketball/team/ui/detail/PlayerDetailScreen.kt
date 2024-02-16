9c86ef45696a9279e7c1f72a79520875eb7aa48c
박상혁 <psh161@naver.com>
2024-02-17 00:29:11 +0900

    BT-02 : Detail Screen Preview

--- app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
+++ app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
@@ -1,5 +1,6 @@
 package co.tami.basketball.team.ui.detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,15 +9,45 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import co.tami.basketball.team.ui.common.DarkLightModePreview
+import co.tami.basketball.team.ui.common.PlayerProfileImage
 import co.tami.basketball.team.ui.common.SystemThemeSurface
+
+
+@Composable
+fun PlayerDetailScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+//        PlayerProfileImage(profileImage = "")
+        PlayerNameText(name = "루카 돈치치")
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            PlayerInfoColumn("31", "AGE", modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(8.dp))
+            PlayerInfoColumn("77", "JERSEY", modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(8.dp))
+            PlayerInfoColumn("2,3", "POSITION", modifier = Modifier.weight(1f))
+        }
+    }
+}
 
 
 @Composable
@@ -40,8 +71,14 @@ fun PlayerInfoColumn(
     Column(
         modifier = modifier
             .aspectRatio(1f)
-            .shadow(elevation = 4.dp)
-            .padding(8.dp),
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.background)
+            .shadow(
+                3.dp,
+                ambientColor = MaterialTheme.colorScheme.onBackground,
+                spotColor = MaterialTheme.colorScheme.onBackground
+
+            ),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
