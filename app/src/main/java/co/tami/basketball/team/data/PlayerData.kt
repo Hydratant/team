40c3d1d5f8ec6d8f6b1ca26853f8909a2fdd0b41
박상혁 <psh161@naver.com>
2024-03-12 00:40:47 +0900

    BT-02 : PlayerData Height 제거

--- app/src/main/java/co/tami/basketball/team/data/PlayerData.kt
+++ app/src/main/java/co/tami/basketball/team/data/PlayerData.kt
@@ -4,7 +4,6 @@ data class PlayerData(
     val id: Long,
     val name: String,
     val age: Int,
-    val height: Int,
     val jersey: Int,
     val position: List<PlayerPosition>,
     val attributes: PlayerAttributesData
