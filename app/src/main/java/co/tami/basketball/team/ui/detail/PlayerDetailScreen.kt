2fa5b00d0c15ddede23374f6fb0a8de469953af6
박상혁 <psh161@naver.com>
2024-03-12 00:08:00 +0900

    BT-02 : Player Info Card 구현

--- app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
+++ app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
@@ -1,31 +1,29 @@
 package co.tami.basketball.team.ui.detail
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import co.tami.basketball.team.ui.common.DarkLightModePreview
-import co.tami.basketball.team.ui.common.PlayerProfileImage
 import co.tami.basketball.team.ui.common.SystemThemeSurface
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayerDetailScreen() {
     Column(
@@ -40,11 +38,11 @@ fun PlayerDetailScreen() {
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
         ) {
-            PlayerInfoColumn("31", "AGE", modifier = Modifier.weight(1f))
+            PlayerInfoCard("31", "AGE", modifier = Modifier.weight(1f))
             Spacer(modifier = Modifier.width(8.dp))
-            PlayerInfoColumn("77", "JERSEY", modifier = Modifier.weight(1f))
+            PlayerInfoCard("77", "JERSEY", modifier = Modifier.weight(1f))
             Spacer(modifier = Modifier.width(8.dp))
-            PlayerInfoColumn("2,3", "POSITION", modifier = Modifier.weight(1f))
+            PlayerInfoCard("2,3", "POSITION", modifier = Modifier.weight(1f))
         }
     }
 }
@@ -62,35 +60,37 @@ fun PlayerNameText(
     )
 }
 
+@ExperimentalMaterial3Api
 @Composable
-fun PlayerInfoColumn(
+fun PlayerInfoCard(
     value: String,
     title: String,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    Card(
         modifier = modifier
             .aspectRatio(1f)
-            .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.background)
-            .shadow(
-                3.dp,
-                ambientColor = MaterialTheme.colorScheme.onBackground,
-                spotColor = MaterialTheme.colorScheme.onBackground
+            .padding(16.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
 
-            ),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = value,
-            style = MaterialTheme.typography.titleMedium
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = title,
-            style = MaterialTheme.typography.titleLarge
-        )
+        ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = value,
+                style = MaterialTheme.typography.displaySmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
     }
 }
 
@@ -102,14 +102,15 @@ fun DetailNamePreview() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @DarkLightModePreview
 @Composable
 fun PlayerInfoColumnPreview() {
     SystemThemeSurface {
         Row(modifier = Modifier.fillMaxWidth()) {
-            PlayerInfoColumn("13", "AGE", modifier = Modifier.weight(1f))
-            PlayerInfoColumn("13", "AGE", modifier = Modifier.weight(1f))
-            PlayerInfoColumn("13", "AGE", modifier = Modifier.weight(1f))
+            PlayerInfoCard("13", "AGE", modifier = Modifier.weight(1f))
+            PlayerInfoCard("181", "Height", modifier = Modifier.weight(1f))
+            PlayerInfoCard("77", "Jersey", modifier = Modifier.weight(1f))
         }
 
     }
