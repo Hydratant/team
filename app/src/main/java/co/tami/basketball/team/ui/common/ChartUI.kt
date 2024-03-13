36fb48ac6d39ade1dd9399836a306c86823f9d4a
박상혁 <psh161@naver.com>
2024-03-14 00:36:06 +0900

    BT-02 :  import 제거

--- app/src/main/java/co/tami/basketball/team/ui/common/ChartUI.kt
+++ app/src/main/java/co/tami/basketball/team/ui/common/ChartUI.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import co.tami.basketball.team.ext.toDp
 import co.tami.basketball.team.ext.toPx
