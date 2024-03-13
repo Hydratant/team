7b53d5f1e0b20ba848c90c75ac6d4dd52c970e78
박상혁 <psh161@naver.com>
2024-03-14 00:40:37 +0900

    BT-02 :  PlayerStats 구현
    - DonutChart

--- app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
+++ app/src/main/java/co/tami/basketball/team/ui/detail/PlayerDetailScreen.kt
@@ -1,6 +1,8 @@
 package co.tami.basketball.team.ui.detail
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,9 +20,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import co.tami.basketball.team.ui.common.DarkLightModePreview
+import co.tami.basketball.team.ui.common.DonutChart
 import co.tami.basketball.team.ui.common.SystemThemeSurface
+import co.tami.basketball.team.ui.common.VerticalSpacer
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,11 +43,20 @@ fun PlayerDetailScreen() {
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
         ) {
+            PlayerInfoCard("89", "OVR", modifier = Modifier.weight(1f))
             PlayerInfoCard("31", "AGE", modifier = Modifier.weight(1f))
-            Spacer(modifier = Modifier.width(8.dp))
-            PlayerInfoCard("77", "JERSEY", modifier = Modifier.weight(1f))
-            Spacer(modifier = Modifier.width(8.dp))
-            PlayerInfoCard("2,3", "POSITION", modifier = Modifier.weight(1f))
+            PlayerInfoCard("77", "Jersey", modifier = Modifier.weight(1f))
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            PlayerStats(stats = 88, statsTitle = "OutSide\nScoring", modifier = Modifier.weight(1f))
+            PlayerStats(stats = 55, statsTitle = "OutSide\nScoring", modifier = Modifier.weight(1f))
+            PlayerStats(stats = 78, statsTitle = "OutSide\nScoring", modifier = Modifier.weight(1f))
         }
     }
 }
@@ -70,7 +84,7 @@ fun PlayerInfoCard(
     Card(
         modifier = modifier
             .aspectRatio(1f)
-            .padding(16.dp),
+            .padding(8.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 10.dp
         ),
@@ -88,10 +102,64 @@ fun PlayerInfoCard(
             Spacer(modifier = Modifier.height(4.dp))
             Text(
                 text = title,
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleMedium
             )
         }
     }
+}
+
+@Composable
+fun PlayerStatusBox(
+    modifier: Modifier = Modifier
+) {
+
+    Box(
+        modifier = modifier
+            .size(300.dp)
+
+    ) {
+        Box(
+            modifier
+                .fillMaxSize()
+                .padding(15.dp)
+                .border(width = 1.dp, color = Color.Blue)
+        )
+        Text(text = "Test")
+
+    }
+
+}
+
+@Composable
+fun PlayerStats(
+    stats: Int,
+    statsTitle: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box {
+            DonutChart(
+                modifier = Modifier.align(Alignment.Center),
+                value = stats.toFloat(),
+                selectColor = MaterialTheme.colorScheme.tertiary,
+                unSelectColor = MaterialTheme.colorScheme.tertiaryContainer
+            )
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = stats.toString(),
+                style = MaterialTheme.typography.displaySmall
+            )
+        }
+        VerticalSpacer(size = 8.dp)
+        Text(
+            text = statsTitle,
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+
 }
 
 @DarkLightModePreview
@@ -113,5 +181,26 @@ fun PlayerInfoColumnPreview() {
             PlayerInfoCard("77", "Jersey", modifier = Modifier.weight(1f))
         }
 
+    }
+}
+
+@DarkLightModePreview
+@Composable
+fun PlayerStatsPreview() {
+    SystemThemeSurface {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            PlayerStats(stats = 88, statsTitle = "OutSide\nScoring", modifier = Modifier.weight(1f))
+            PlayerStats(stats = 55, statsTitle = "OutSide\nScoring", modifier = Modifier.weight(1f))
+            PlayerStats(stats = 78, statsTitle = "OutSide\nScoring", modifier = Modifier.weight(1f))
+        }
+
+    }
+}
+
+@DarkLightModePreview
+@Composable
+fun PlayerStatusBoxPreview() {
+    SystemThemeSurface {
+        PlayerStatusBox()
     }
 }
