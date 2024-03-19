5af4709f998b212a1c74f075cd12b76fa0a818be
박상혁 <psh161@naver.com>
2024-03-20 00:12:01 +0900

    BT-04 : RadarLineData 추가

---
+++ app/src/main/java/co/tami/basketball/team/ui/chart/radar/data/RadarLineData.kt
@@ -0,0 +1,11 @@
+package co.tami.basketball.team.ui.chart.radar.data
+
+import android.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+
+
+data class RadarLineData(
+    val lineColor: Color,
+    val strokeWidth: Float,
+    val strokeCap: StrokeCap
+)
