650b32634c4dde2285870761b861e58f58e2bc03
박상혁 <psh161@naver.com>
2024-03-21 00:27:33 +0900

    BT-04 : Canvas 로  정 다각형 구현 주석 추가

--- app/src/main/java/co/tami/basketball/team/ui/chart/radar/RadarChart.kt
+++ app/src/main/java/co/tami/basketball/team/ui/chart/radar/RadarChart.kt
@@ -1,7 +1,6 @@
 package co.tami.basketball.team.ui.chart.radar
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,11 +18,13 @@ fun DrawLines(
     num: Int
 ) {
     Canvas(modifier = modifier) {
+        // Canvas 중앙 Offset
         val center = Offset(size.width / 2, size.height / 2)
-        val radius = size.minDimension / 2f
-        val angleBetweenLines = PI * 2 / num
-        val angleOfFirstLine = 0 * angleBetweenLines
-        val offsetAngle = -PI / 2 - angleOfFirstLine
+        val radius = size.minDimension / 2f // 반지름
+        val angleBetweenLines = PI * 2 / num // 호도법에 의해서 PI * 2 는 360
+
+        // 꼭지점을 구하기 위한 offset -PI / 2 는 -180도기 때문에 무조건 12시 방향부터 시작.
+        val offsetAngle = -PI / 2
 
         var startOffset = Calculator.getCircumferencePointOffset(
             center, radius, offsetAngle
@@ -32,7 +33,7 @@ fun DrawLines(
             val endOffset = Calculator.getCircumferencePointOffset(
                 center,
                 radius,
-                (i + 1) * angleBetweenLines + offsetAngle
+                (i + 1) * angleBetweenLines  + offsetAngle
             )
             drawLine(
                 color = Color.Blue,
@@ -53,8 +54,7 @@ fun DrawLinesPreview() {
 
     DrawLines(
         modifier = Modifier
-            .size(300.dp)
-            .padding(16.dp),
-        5
+            .size(300.dp),
+        6
     )
 }
