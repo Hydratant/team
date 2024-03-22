f9b80fce36a499569abaf501d658f5750f3207e9
박상혁 <psh161@naver.com>
2024-03-22 17:43:52 +0900

    BT-04 : Canvas Radar Chart 구현
    - 소스정리 필요

--- app/src/main/java/co/tami/basketball/team/ui/chart/radar/RadarChart.kt
+++ app/src/main/java/co/tami/basketball/team/ui/chart/radar/RadarChart.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,48 +12,59 @@ import kotlin.math.PI
 
 
 @Composable
-fun DrawLines(
-    modifier: Modifier = Modifier,
-    num: Int
+fun DrawPolygonLine(
+    vertexCount: Int,
+    statCount:Int,
+    modifier: Modifier = Modifier
 ) {
     Canvas(modifier = modifier) {
-        // Canvas 중앙 Offset
-        val center = Offset(size.width / 2, size.height / 2)
         val radius = size.minDimension / 2f // 반지름
-        val angleBetweenLines = PI * 2 / num // 호도법에 의해서 PI * 2 는 360
 
+        val drawRadius = radius / statCount
         // 꼭지점을 구하기 위한 offset -PI / 2 는 -180도기 때문에 무조건 12시 방향부터 시작.
         val offsetAngle = -PI / 2
+        val angleBetweenLines = PI * 2 / vertexCount // 호도법에 의해서 PI * 2 는 360
 
-        var startOffset = Calculator.getCircumferencePointOffset(
-            center, radius, offsetAngle
-        )
-        for (i in 0 until num) {
-            val endOffset = Calculator.getCircumferencePointOffset(
-                center,
-                radius,
-                (i + 1) * angleBetweenLines  + offsetAngle
+        for (i in 1..statCount) {
+            val calculatorRadius = drawRadius * i
+
+            var startOffset = Calculator.getCircumferencePointOffset(
+                center, calculatorRadius, offsetAngle
             )
-            drawLine(
-                color = Color.Blue,
-                start = startOffset,
-                end = endOffset,
-                strokeWidth = 2f,
-                cap = StrokeCap.Round
-            )
-            startOffset = endOffset
+            for (i in 0 until vertexCount) {
+                val endOffset = Calculator.getCircumferencePointOffset(
+                    center,
+                    calculatorRadius,
+                    (i + 1) * angleBetweenLines + offsetAngle
+                )
+                drawLine(
+                    color = Color.Blue,
+                    start = startOffset,
+                    end = endOffset,
+                    strokeWidth = 2f,
+                    cap = StrokeCap.Round
+                )
+                startOffset = endOffset
+                drawLine(
+                    color = Color.Gray,
+                    start = center,
+                    end = startOffset,
+                    strokeWidth = 2f,
+                    cap = StrokeCap.Round
+                )
+            }
         }
     }
 }
 
-
 @Composable
 @Preview
-fun DrawLinesPreview() {
+fun DrawPolygonLinePreview() {
 
-    DrawLines(
+    DrawPolygonLine(
+        8,
+        5,
         modifier = Modifier
-            .size(300.dp),
-        6
+            .size(300.dp)
     )
 }
