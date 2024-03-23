c57072d17870ac262ba4b2694f222b82645a53e7
박상혁 <psh161@naver.com>
2024-03-23 15:50:59 +0900

    BT-04 : Radar Chart Line 그리기 소스 정리

--- app/src/main/java/co/tami/basketball/team/ui/chart/radar/RadarChart.kt
+++ app/src/main/java/co/tami/basketball/team/ui/chart/radar/RadarChart.kt
@@ -7,51 +7,67 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import co.tami.basketball.team.ext.toPx
 import kotlin.math.PI
 
 
+private val DEFAULT_STROKE_CAP: StrokeCap = StrokeCap.Round
+private val DEFAULT_STORK_SIZE: Dp = 1.dp
+
 @Composable
 fun DrawPolygonLine(
+    lineColor: Color,
     vertexCount: Int,
-    statCount:Int,
-    modifier: Modifier = Modifier
+    statCount: Int,
+    modifier: Modifier = Modifier,
+    strokeWidth: Float = DEFAULT_STORK_SIZE.toPx(),
+    strokeCap: StrokeCap = DEFAULT_STROKE_CAP
 ) {
     Canvas(modifier = modifier) {
         val radius = size.minDimension / 2f // 반지름
 
-        val drawRadius = radius / statCount
-        // 꼭지점을 구하기 위한 offset -PI / 2 는 -180도기 때문에 무조건 12시 방향부터 시작.
+        val angleBetweenLines = PI * 2 / vertexCount // 정 다각형 꼭짓점을 찍기 위해 각도를 구한다.
         val offsetAngle = -PI / 2
-        val angleBetweenLines = PI * 2 / vertexCount // 호도법에 의해서 PI * 2 는 360
+
+        // StatCount 로 나눈다.
+        val startDrawRadius = radius / statCount
 
         for (i in 1..statCount) {
-            val calculatorRadius = drawRadius * i
-
+            val calculatorRadius = startDrawRadius * i
             var startOffset = Calculator.getCircumferencePointOffset(
                 center, calculatorRadius, offsetAngle
             )
-            for (i in 0 until vertexCount) {
+
+            for (vertexIndex in 1..vertexCount) {
+                val endOffsetAngle = angleBetweenLines * vertexIndex + offsetAngle
                 val endOffset = Calculator.getCircumferencePointOffset(
                     center,
                     calculatorRadius,
-                    (i + 1) * angleBetweenLines + offsetAngle
+                    endOffsetAngle
                 )
+                // 정 다각형 그리기
                 drawLine(
-                    color = Color.Blue,
+                    color = lineColor,
                     start = startOffset,
                     end = endOffset,
-                    strokeWidth = 2f,
-                    cap = StrokeCap.Round
+                    strokeWidth = strokeWidth,
+                    cap = strokeCap
                 )
                 startOffset = endOffset
-                drawLine(
-                    color = Color.Gray,
-                    start = center,
-                    end = startOffset,
-                    strokeWidth = 2f,
-                    cap = StrokeCap.Round
-                )
+
+                // 원의 중심에서 꼭짓점까지 선을 그리기
+                // 한번만 그리기 위해 마지막 Stat 다각형을 그릴때 그린다.
+                if (i == statCount) {
+                    drawLine(
+                        color = lineColor,
+                        start = center,
+                        end = startOffset,
+                        strokeWidth = strokeWidth,
+                        cap = strokeCap
+                    )
+                }
             }
         }
     }
@@ -62,6 +78,7 @@ fun DrawPolygonLine(
 fun DrawPolygonLinePreview() {
 
     DrawPolygonLine(
+        Color.Gray,
         8,
         5,
         modifier = Modifier
