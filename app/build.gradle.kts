952177912825c9ed8238557726bbe26ad9e7a009
박상혁 <psh161@naver.com>
2024-04-20 16:17:43 +0900

    BT-06 : Kotlin Serialization 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt)
     alias(libs.plugins.google.services)
 }
