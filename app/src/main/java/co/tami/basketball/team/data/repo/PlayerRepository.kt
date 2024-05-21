7f5e7c513d10f256df33d77e7c10f287d2910e76
박상혁 <psh161@naver.com>
2024-05-21 23:33:23 +0900

    feat : Repository getPlayers 구현

--- app/src/main/java/co/tami/basketball/team/data/repo/PlayerRepository.kt
+++ app/src/main/java/co/tami/basketball/team/data/repo/PlayerRepository.kt
@@ -1,7 +1,10 @@
 package co.tami.basketball.team.data.repo
 
 import co.tami.basketball.team.domain.entity.PlayerEntity
+import kotlinx.coroutines.flow.Flow
 
 interface PlayerRepository {
+
+    fun getPlayers(): Flow<List<PlayerEntity>>
     suspend fun getPlayer(id: Long): PlayerEntity
 }
--- app/src/main/java/co/tami/basketball/team/data/repo/PlayerRepositoryImpl.kt
+++ app/src/main/java/co/tami/basketball/team/data/repo/PlayerRepositoryImpl.kt
@@ -1,12 +1,19 @@
 package co.tami.basketball.team.data.repo
 
+import co.tami.basketball.team.data.PlayerData
 import co.tami.basketball.team.data.source.PlayerDataSource
 import co.tami.basketball.team.data.toEntity
 import co.tami.basketball.team.domain.entity.PlayerEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class PlayerRepositoryImpl(
     private val playerDataSource: PlayerDataSource
 ) : PlayerRepository {
+    override fun getPlayers(): Flow<List<PlayerEntity>> = flow {
+        emit(playerDataSource.getPlayers().map { data: PlayerData -> data.toEntity() })
+    }
+
     override suspend fun getPlayer(id: Long): PlayerEntity =
         playerDataSource.getPlayer(id).toEntity()
 }
