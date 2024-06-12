57164924ffd182a07c0783087c3498b4851ebab4
박상혁 <psh161@naver.com>
2024-06-12 22:34:35 +0900

    BT-14 : StateIn으로 변경

--- app/src/main/java/co/tami/basketball/team/ui/list/PlayerListViewModel.kt
+++ app/src/main/java/co/tami/basketball/team/ui/list/PlayerListViewModel.kt
@@ -6,28 +6,24 @@ import co.tami.basketball.team.data.repo.PlayerRepository
 import co.tami.basketball.team.domain.entity.PlayerEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PlayerListViewModel @Inject constructor(
-    private val playerRepository: PlayerRepository
+    playerRepository: PlayerRepository
 ) : ViewModel() {
 
-    private val _players = MutableStateFlow<List<PlayerEntity>>(emptyList())
-    val players: StateFlow<List<PlayerEntity>>
-        get() = _players.asStateFlow()
+    val players: StateFlow<List<PlayerEntity>> =
+        playerRepository.getPlayers()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = emptyList()
+            )
 
-    init {
-        fetchPlayers()
-    }
-
-    private fun fetchPlayers() {
-        viewModelScope.launch {
-            playerRepository.getPlayers()
-                .collect { players -> _players.value = players }
-        }
-    }
 }
