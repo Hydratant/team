1560ca6b13a092e929317c6712e134a4a3460238
박상혁 <psh161@naver.com>
2024-06-12 22:34:55 +0900

    BT-14 : import 제거

--- app/src/main/java/co/tami/basketball/team/ui/list/PlayerListViewModel.kt
+++ app/src/main/java/co/tami/basketball/team/ui/list/PlayerListViewModel.kt
@@ -5,12 +5,9 @@ import androidx.lifecycle.viewModelScope
 import co.tami.basketball.team.data.repo.PlayerRepository
 import co.tami.basketball.team.domain.entity.PlayerEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
