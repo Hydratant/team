092380e8e5775432e48e45508395ced026cd6148
박상혁 <psh161@naver.com>
2024-06-21 17:04:54 +0900

    BT-14 : PlayerUI PlayerProfile 변경

--- app/src/main/java/co/tami/basketball/team/ui/common/PlayerUI.kt
+++ app/src/main/java/co/tami/basketball/team/ui/common/PlayerUI.kt
@@ -1,6 +1,8 @@
 package co.tami.basketball.team.ui.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,8 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import co.tami.basketball.team.R
 
 
 @ExperimentalMaterial3Api
@@ -66,6 +75,22 @@ fun PlayerInfoCard(
     }
 }
 
+
+@Composable
+fun PlayerProfile(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier
+            .size(70.dp)
+            .aspectRatio(1f)
+            .clip(CircleShape)
+            .border(1.dp, Color.Gray, CircleShape),
+        contentScale = ContentScale.Inside,
+        painter = painterResource(id = R.drawable.luka_profile),
+        contentDescription = null
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @DarkLightModePreview
