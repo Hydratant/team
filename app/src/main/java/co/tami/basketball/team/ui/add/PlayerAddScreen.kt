f619a33e072b7da918ea0373a24000a11d7615d4
박상혁 <psh161@naver.com>
2024-06-29 14:05:33 +0900

    BT-14 : PlayerNameTextField 추가

--- app/src/main/java/co/tami/basketball/team/ui/add/PlayerAddScreen.kt
+++ app/src/main/java/co/tami/basketball/team/ui/add/PlayerAddScreen.kt
@@ -1,8 +1,40 @@
 package co.tami.basketball.team.ui.add
 
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import co.tami.basketball.team.R
+import co.tami.basketball.team.ui.common.DarkLightModePreview
+import co.tami.basketball.team.ui.common.SystemThemeSurface
 
 @Composable
 fun PlayerScreen() {
 
+}
+
+@Composable
+fun PlayerNameTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        placeholder = { Text(text = stringResource(id = R.string.player_name_placeholder)) },
+        onValueChange = onValueChange
+    )
+}
+
+@DarkLightModePreview
+@Composable
+fun PlayerNameTextFieldPreview() {
+    SystemThemeSurface {
+        PlayerNameTextField(
+            value = "",
+            onValueChange = {}
+        )
+    }
 }
